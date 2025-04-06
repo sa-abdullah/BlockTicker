6b3470e73818631289fec8749f8c42542cce0796
sa-abdullah <salaudeenabdu@gmail.com>
2025-04-06 23:59:16 +0100

    Reduced global fonts size on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {},
+    fontSize: {
+      'sm': '0.75rem',   // smaller text
+      'base': '0.85rem', // smaller default
+      'lg': '1rem',      // smaller large text
+      'xl': '1.25rem',
+    },
   },
   plugins: [],
 }
