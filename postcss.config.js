b4d67d01ce6c1cc288eb3aed664510a2e65fb42f
sa-abdullah <salaudeenabdu@gmail.com>
2025-04-06 23:22:53 +0100

    modified several files

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-import tailwindcss from '@tailwindcss';
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import autoprefixer from "autoprefixer";
-import tailwindcssPlugin from "@tailwindcss/postcss"; // <-- IMPORTANT
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcssPlugin(), autoprefixer()],
-    },
-  },
-});
+  plugins: [react()]
+})
