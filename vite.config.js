6c12458f97325771fe88be8ae7972250535ea316
sa-abdullah <salaudeenabdu@gmail.com>
2025-04-06 22:41:17 +0100

    new change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
-import tailwindcssPlugin from "@tailwindcss/postcss"; 
+import tailwindcssPlugin from "@tailwindcss/postcss"; // <-- IMPORTANT
 
 export default defineConfig({
   plugins: [react()],
