78714fd3424fd849a2cf0eb446eed7c15e60099c
sa-abdullah <salaudeenabdu@gmail.com>
2025-04-07 00:13:21 +0100

    new change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,6 @@ export default {
   ],
   theme: {
     extend: {},
-    fontSize: {
-      'sm': '0.75rem',
-      'base': '0.85rem',
-      'lg': '1rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.125rem', 
-      '5xl': '2.5rem',
-    }
-    
   },
   plugins: [],
 }
